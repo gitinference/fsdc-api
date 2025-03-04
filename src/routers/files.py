4e69605cbbb6289060a2fa893f8a7b0762323574
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-03-04 20:08:57 +0000

    21 add maps (#22)
    * Staging (#20)
    
    * fixed run path of the docker image (#19)
    
    * Update and rename dockerfile to Dockerfile
    
    * fix stuff
    
    * fixe issue with submoduels
    
    * update build script
    
    * added security submodule
    
    * added routs for fsdc security
    
    * added support for api

--- src/routers/files.py
+++ src/routers/files.py
@@ -1,15 +1,24 @@
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
 from ..submodules.fsdc_calories.src.data_process import DataCal
+from ..submodules.fsdc_security.src.data.data_viz import DataSecurity
+
 import os
 
 router = APIRouter()
-dc = DataCal()
 
 
 @router.get("/files/nutrition/")
 async def get_calaries_data():
-    df = dc.gen_nuti_data()
-    file_path = os.path.join(os.getcwd(), "data", "nutrition.csv")
+    df = DataCal().gen_nuti_data()
+    file_path = os.path.join(os.getcwd(), "data", "processed", "nutrition.csv")
     df.write_csv(file_path)
-    return FileResponse(file_path, media_type="text/csv", filename=f"nutrition.csv")
+    return FileResponse(file_path, media_type="text/csv", filename="nutrition.csv")
+
+
+@router.get("/files/nutrition/")
+async def get_security_data():
+    df = DataSecurity().calc_security()
+    file_path = os.path.join(os.getcwd(), "data", "processed", "security.csv")
+    df.write_csv(file_path)
+    return FileResponse(file_path, media_type="text/csv", filename="security.csv")
