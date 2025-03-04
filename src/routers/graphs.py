3df215ef78af7ae1de090666668efef58356c257
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-03-04 13:29:25 -0400

    added routs for fsdc security

--- src/routers/graphs.py
+++ src/routers/graphs.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter
 from ..submodules.fsdc_calories.src.data_process import DataCal
+from ..submodules.fsdc_security.src.data.data_viz import DataSecurity
 
 router = APIRouter()
-
-dc = DataCal()
 
 
 @router.get("/graph/nutrition")
 async def get_calaries_data():
-    return dc.gen_graphs().to_dict()
+    return DataCal().gen_graphs().to_dict()
+
+
+@router.get("/graph/security")
+async def gen_security_graph(year: int, var: str):
+    chart = DataSecurity().gen_graph(
+        year=year, var=var, type="linear", title="Security Map"
+    )
+    return chart.to_dict()
