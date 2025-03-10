279e7cdb24c5d717a0ea115b156cdb44f74cc165
joel-alvarado <joel.alvarado2@upr.edu>
2025-03-09 23:03:46 -0400

    Add documentation for graph parameters for SwaggerUI

--- src/routers/graphs.py
+++ src/routers/graphs.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 
@@ -8,6 +10,18 @@ from ..submodules.pr_food.src.data.data_process import FoodDeseart
 router = APIRouter()
 
 
+class SecurityGraphModel(str, Enum):
+    total_insec = "total_insec"
+    insecurity_hous = "insecurity_hous"
+
+
+class FoodGraphModel(str, Enum):
+    supermarkets_and_others = "supermarkets_and_others"
+    supermarkets = "supermarkets"
+    convenience_retailers = "convenience_retailers"
+    whole_foods = "whole_foods"
+
+
 @router.get("/graph/nutrition", response_class=HTMLResponse)
 async def get_calaries_data():
     return DataCal().gen_graphs_nuti_data().to_html()
@@ -15,18 +29,19 @@ async def get_calaries_data():
 
 @router.get("/graph/security", response_class=HTMLResponse)
 # the available variables
-async def gen_security_graph(year: int, var: str):
+async def gen_security_graph(year: int, var: SecurityGraphModel):
     chart = DataSecurity().gen_graph(
-        year=year, var=var, type="linear", title="Security Map"
+        year=year, var=var.value, type="linear", title="Security Map"
     )
     return chart.to_html()
 
 
 @router.get("/graph/food", response_class=HTMLResponse)
-# the available variables are supermarkets_and_others supermarkets convenience_retailers and whole_foods
-async def gen_food_graph(var: str, year: int, qtr: int, title):
+async def gen_food_graph(var: FoodGraphModel, year: int, qtr: int, title):
     return (
-        FoodDeseart().gen_food_graph(var=var, year=year, qtr=qtr, title=title).to_html()
+        FoodDeseart()
+        .gen_food_graph(var=var.value, year=year, qtr=qtr, title=title)
+        .to_html()
     )
 
 
